56857844271e1eb840b7a8a795d0782f530c3933
soleperson <h2magic@126.com>
2020-01-25 21:43:30 +0800

    Ignore directory: cmake-build-debug, .idea

---
+++ Model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 寰炸天 on 2020/1/25.
+//
+
+#include "Model.h"
---
+++ Model.h
@@ -0,0 +1,16 @@
+//
+// Created by 寰炸天 on 2020/1/25.
+//
+
+#ifndef SOLEPERSONORM_MODEL_H
+#define SOLEPERSONORM_MODEL_H
+
+#include <string>
+#include <iostream>
+
+class BaseModel
+{
+
+};
+
+#endif //SOLEPERSONORM_MODEL_H
